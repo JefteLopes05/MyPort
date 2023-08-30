5272966ecb6f48ac94a816d22db25d39b872c1ba
Jefte Lopes <joao.pedro@moldsoft.com.br>
2023-08-30 13:23:30 -0300

    Versão Ajustada

--- webapp/controller/Menu.controller.js
+++ webapp/controller/Menu.controller.js
@@ -2,37 +2,39 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/Device",
 	"sap/ui/model/json/JSONModel"
-], function(
+], function (
 	Controller, Device, JSONModel
 ) {
 	"use strict";
 
 	return Controller.extend("sap.devs.port.myport.controller.Menu", {
 		onInit: function () {
-			var oProductsModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json")),
-				iPagesCount = 1;
 
-			if (Device.system.desktop) {
-				iPagesCount = 4;
-			} else if (Device.system.tablet) {
-				iPagesCount = 2;
-			}
+			var oModel = new JSONModel({
 
-			var oSettingsModel = new JSONModel({ pagesCount: iPagesCount });
-			oProductsModel.setSizeLimit(6);
+			});
 
-			this.getView().setModel(oSettingsModel, "settings");
-			this.getView().setModel(oProductsModel, "products");
+			this.getView().setModel(oModel);
 		},
 
 
-		onRedirectToLinkedin: function(){
+		onDownloadPDFCurriculum: function () {
+			var pdfUrl = "image/LogoLinkedin.png"; // Substitua pelo URL real do seu PDF
+			var fileName = "LogoLinkedin.png"; // Nome do arquivo a ser baixado
+
+			var link = document.createElement("a");
+			link.href = pdfUrl;
+			link.download = fileName;
+			link.click();
+		},
+
+		onRedirectToLinkedin: function () {
 			var linkedinProfileURL = "https://www.linkedin.com/in/jeft%C3%A9-lopes-5a3776251";
-            window.open(linkedinProfileURL);
+			window.open(linkedinProfileURL);
 		},
-		onRedirectToGitHub: function(){
+		onRedirectToGitHub: function () {
 			var linkedinProfileURL = "https://github.com/JefteLopes05";
-            window.open(linkedinProfileURL);
+			window.open(linkedinProfileURL);
 		}
 	});
 });
